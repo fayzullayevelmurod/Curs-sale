33cd5cb6c0c6d274ae8d431450aa831820e35cdb
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-02-29 07:07:25 +0500

    media the end

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 8,
     breakpoints: {
         320: {
-          slidesPerView: 2,
+          slidesPerView: 1.65,
         },
         768: {
-          slidesPerView: 4,
+          slidesPerView: 3.65,
         },
         1024: {
-          slidesPerView: 4.50,
+          slidesPerView: 4.65,
         },
       },
 });
