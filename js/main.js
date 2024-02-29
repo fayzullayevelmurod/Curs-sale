d7d0ce4e66582c39f2835121526f1068502c6ae9
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-02-29 19:09:12 +0500

    media the end

--- js/main.js
+++ js/main.js
@@ -46,6 +46,7 @@ main_btns.forEach(item => {
 
 checkRadio();
 
+
 function checkRadio() {
   main_btns.forEach(item => {
 
@@ -57,3 +58,68 @@ function checkRadio() {
     }
   })
 }
+
+
+let option_card = document.querySelectorAll('.option_cards label');
+
+option_card.forEach(item2 => {
+  item2.addEventListener('click', () => {
+    checkOption()
+  })
+})
+
+checkOption();
+
+
+function checkOption() {
+  option_card.forEach(item2 => {
+
+    let input = item2.querySelector('input')
+    if(input.checked) {
+      item2.classList.add('active')
+    } else {
+      item2.classList.remove('active')
+    }
+  })
+}
+
+let notification_one_btn = document.querySelector('.notification_one_btn');
+let modal_main = document.querySelector('.modal_main');
+let modal_bg = document.querySelector('.modal_bg');
+let left_btn = document.querySelector('.left_btn');
+let modal_fine = document.querySelector('.modal_fine');
+let modal_fine_btn = document.querySelector('.modal_fine_btn');
+let notification_two_btn = document.querySelector('.notification_two_btn');
+let modal_close = document.querySelector('.modal_close');
+let modal_close_btn = document.querySelector('.modal_close_btn');
+
+notification_one_btn.addEventListener('click', () => {
+  modal_main.classList.toggle('active')
+  modal_bg.classList.toggle('active')
+})
+
+modal_bg.addEventListener('click', () => {
+  modal_bg.classList.remove('active')
+  modal_main.classList.remove('active')
+})
+
+left_btn.addEventListener('click', () => {
+  modal_fine.classList.add('active')
+  modal_main.classList.remove('active')
+  modal_bg.classList.add('active')
+})
+
+modal_fine_btn.addEventListener('click', () => {
+  modal_fine.classList.remove('active')
+  modal_bg.classList.remove('active')
+})
+
+notification_two_btn.addEventListener('click', () => {
+  modal_close.classList.add('active')
+  modal_bg.classList.add('active')
+})
+
+modal_close_btn.addEventListener('click', () => {
+  modal_bg.classList.remove('active')
+  modal_close.classList.remove('active')
+})
