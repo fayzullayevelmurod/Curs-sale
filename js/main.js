51fd690e66e0dcbcb977683935fd8dd9e9a8bf5b
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-02-29 09:05:09 +0500

    media the end

--- js/main.js
+++ js/main.js
@@ -18,3 +18,18 @@ lightGallery(document.getElementById('lightgallery'), {
     licenseKey: 'your_license_key',
     speed: 500,
 });
+
+lightGallery(document.getElementById('lightgallerytwo'), {
+    plugins: [lgZoom],
+    licenseKey: 'your_license_key',
+    speed: 500,
+});
+
+
+let header_under_btn = document.querySelector('.header_under_btn');
+let header_under_content = document.querySelector('.header_under_content');
+
+header_under_btn.addEventListener('click', () => {
+  header_under_content.classList.toggle('active')
+  header_under_btn.classList.toggle('active')
+})
